3c9ead1c99814114d7be12573ad516223c5bc68a
Jesse <jessethegauntt+biz@gmail.com>
2023-02-21 23:09:44 -0800

    Upcoming meeting notes, some flare

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,8 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-12">
-          <div
+          
+			<div v-if="onTime"
             class="alert alert-warning alert-dismissible fade show mt-2"
             role="alert"
           >
@@ -54,9 +55,50 @@
             </p>
 
             <div class="mt-3 ml-3 mr-3">
-              <!-- <pre style="background: #FFF0D5; border: 1px black solid; padding: 20px; text-align: left;">
-	<h2>Upcoming Meeting Agenda for Tuesday, Feb 7th</h2>
-</pre> -->
+<pre style="background: #FFF0D5; border: 1px black solid; padding: 20px; text-align: left; white-space: pre-wrap;" v-if="onTime">
+<h3>Upcoming Meeting Agenda for Tuesday, March 7th</h3>
+
+<b>1)	Meeting Called to order</b>
+a.	Board members present
+b.	Welcome guests
+
+<b>2)	Meeting Minutes Approval from </b>
+a.	02/07/2022 Meeting
+
+<b>3)	Report of Administrator</b>
+a.	Attachments
+
+<b>4)	Old Business</b>
+a.	Contact email addresses
+b.	Bylaw's Committee Update
+c.	Building Committee Updated 
+i.	New building application
+d.	Community Newsletter
+i.	Social media, quarterly and email
+e.	HPOA Board Code of Ethics
+f.	Insurance policy
+g.	Storage unit
+h.	WM Garbage Service
+i.	Covenants Enforcement
+j.	Interim Access through Private Property at 1041 Snoqualmie Drive
+k.	Forest Service Termination of a Segment of an Easement 
+
+<b>5)	New Business</b>
+a.	Recording of HPOA Board Meetings 
+b.	Hiring a second Legal Representation specializing in HOA 
+c.	Senate Bill 5727 
+
+<b>6)	Communications</b>
+
+<b>7)	Guest Comments</b>
+
+<b>8)	Building Committee: (Building applications are accepted up to 10 days prior to meetings)</b>
+a.	Lot 54 Gagnon
+
+<b>9)	Meeting Adjourned </b>
+a.	Next Meeting Date: 04/04/2023
+
+</pre>
             </div>
 
             <HyakMeetings />
@@ -70,12 +112,23 @@
 <script lang="ts">
 // @ is an alias to /src
 import HyakMeetings from "@/components/HyakMeetings.vue";
+import moment from "moment";
 
 export default {
   name: "HomeView",
   components: {
     HyakMeetings,
   },
+
+  computed: {
+
+	// only show the upcoming agenda until a little after the meeting date (March 7th)
+	onTime() {
+		const now = moment();
+		const later = moment('2023-03-07 02:00');
+		return now.isBefore(later);
+	}
+  }
 };
 </script>
 
