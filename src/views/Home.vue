d1184b68b43a4e2ab43bc2a3089031c01c1e4479
Jesse <jessethegauntt+biz@gmail.com>
2023-03-22 13:25:28 -0700

    Probably fine

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,10 @@
         <div class="col-lg-12">
           
 			<div v-if="onTime"
-            class="alert alert-warning alert-dismissible fade show mt-2"
+            class="alert alert-info alert-dismissible fade show mt-2"
             role="alert"
           >
-            The next meeting is March 7th at 6:30pm
+            The next meeting is April 4th at 6:30pm
 
             <button
               type="button"
@@ -56,47 +56,53 @@
 
             <div class="mt-3 ml-3 mr-3">
 <pre style="background: #FFF0D5; border: 1px black solid; padding: 20px; text-align: left; white-space: pre-wrap;" v-if="onTime">
-<h3>Upcoming Meeting Agenda for Tuesday, March 7th</h3>
+<h3>Upcoming Meeting Agenda for Tuesday, April 4th</h3>
 
-<b>1)	Meeting Called to order</b>
-a.	Board members present
-b.	Welcome guests
+1) Meeting Called to order
+	a. Board members present
+	b. Welcome guests
 
-<b>2)	Meeting Minutes Approval from </b>
-a.	02/07/2022 Meeting
+2) Meeting Minutes Approval from last meeting
 
-<b>3)	Report of Administrator</b>
-a.	Attachments
+3) Report of Administrator
+	a. Attachments
+	b. Annual Budget
+	c. Emailing and mailing invoices
 
-<b>4)	Old Business</b>
-a.	Contact email addresses
-b.	Bylaw's Committee Update
-c.	Building Committee Updated 
-i.	New building application
-d.	Community Newsletter
-i.	Social media, quarterly and email
-e.	HPOA Board Code of Ethics
-f.	Insurance policy
-g.	Storage unit
-h.	WM Garbage Service
-i.	Covenants Enforcement
-j.	Interim Access through Private Property at 1041 Snoqualmie Drive
-k.	Forest Service Termination of a Segment of an Easement 
+4) Old Business
+	a. Contact email addresses
+	b. Bylaw's Committee Update
+	i. STVR and Building Committee Building Guidelines Surveys
+	c. Building Committee Updated
+	i. New building application
+	d. Community Newsletter
+	i. Social media, quarterly and email
+	e. HPOA Board Code of Ethics
+	f. Insurance policy (updating current policy)
+	g. Storage unit
+	h. WM Garbage Service
+	i. Covenants Enforcement Policy
+	j. Division 1 Lot 97, 98, 99 relevant document discussion
 
-<b>5)	New Business</b>
-a.	Recording of HPOA Board Meetings 
-b.	Hiring a second Legal Representation specializing in HOA 
-c.	Senate Bill 5727 
+5) New Business
+	a. Recording of HPOA Board Meetings
+	b. Hiring a second Legal Representation specializing in HOA
+	c. Senate Bill <a style="text-decoration: underline;" target="_blank" href="https://app.leg.wa.gov/billsummary?BillNumber=5727&Year=2023">5727</a>
+	d. Qualifying Quorum's and Cancelations
+	e. Social Media Policy and new member approval timelines
+	f. Annual Meeting and Neighborhood cleanup - dates and budget details
+	g. Annual Mailer and Ballots - positions up for election
 
-<b>6)	Communications</b>
+6) Communications
 
-<b>7)	Guest Comments</b>
+7) Guest Comments
 
-<b>8)	Building Committee: (Building applications are accepted up to 10 days prior to meetings)</b>
-a.	Lot 54 Gagnon
+8) Building Committee: (Building applications are accepted up to 10 days prior to meetings)
+	a. Lot 80 Hill
+	b. Lot 54 Gagnon
 
-<b>9)	Meeting Adjourned </b>
-a.	Next Meeting Date: 04/04/2023
+9) Meeting Adjourned
+	a. Next Meeting Date: 05/02/2023
 
 </pre>
             </div>
@@ -125,7 +131,7 @@ export default {
 	// only show the upcoming agenda until a little after the meeting date (March 7th)
 	onTime() {
 		const now = moment();
-		const later = moment('2023-03-07 02:00');
+		const later = moment('2023-04-05 02:00');
 		return now.isBefore(later);
 	}
   }
