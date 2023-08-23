26ce8cfb442d545e2c13bce8c9ca11f6f6632fdb
Jesse <jessethegauntt+biz@gmail.com>
2023-08-23 12:14:19 -0700

    Sickest

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,7 +13,7 @@
             class="alert alert-info alert-dismissible fade show mt-2"
             role="alert"
           >
-            The next meeting is Aug 1st at 6:30pm
+            The next meeting is Sep 5th at 6:30pm
 
             <button
               type="button"
@@ -69,51 +69,47 @@
                 "
                 v-if="onTime"
               >
-<h3>Upcoming Meeting Agenda for Tuesday, Aug 1st</h3>
-1)	Meeting Called to order 
-	a.	Board members present
-	b.	Welcome guests
+<h3>Upcoming Meeting Agenda for Tuesday, Sep 5th</h3>
+1) Meeting Called to order
+a. Board members present
+b. Welcome guests
 
-2)	New Board Members
-	a.	Appointment of Board Member Vacancy per Article IV Section 7: 
-		i.	Any vacancy occurring in the Board of Trustees shall be filled by appointment by a majority of the remaining Trustees. The person so appointed shall hold office until the next regular meeting of the members of the Corporation at which Annual or adjourned Annual Meeting the vacancies for the remainder of the original terms, if any, shall be filled by election by the members in the regular manner.
-	b.	Appointment of Treasurer Position 
+2) New Board Members
+a. Appointment of Board Member Vacancy per Article IV Section 7:
+	i. Any vacancy occurring in the Board of Trustees shall be filled by appointment by a
+	majority of the remaining Trustees. The person so appointed shall hold office until the
+	next regular meeting of the members of the Corporation at which Annual or adjourned
+	Annual Meeting the vacancies for the remainder of the original terms, if any, shall be
+	filled by election by the members in the regular manner.
 
-3)	Meeting Minutes Approval from 
-	a.	06/06/2022 Meeting
+3) Meeting Minutes Approval from
+a. 08/01/2022 Meeting
 
-4)	Report of Administrator
-	a.	Attachments
-	b.	Annual Budget
-	c.	Past dues over $250.00 Lien file for 3 lots
+4) Report of Administrator
+a. Attachments
 
-5)	Building Committee: (Building applications are accepted up to 10 days prior to meetings)
-	a.	None submitted by 7.18.2023
+5) Building Committee: (Building applications are accepted up to 10 days prior to meetings)
+a. None submitted by 8.22.2023
 
-6)	Old Business
-	a.	Bylaw’s Committee Update
-	b.	Building Committee Updated 
-	c.	Insurance policy (updating current policy)
-	d.	Annual Meeting and Neighborhood cleanup
-		i.	Annual Mailer reimbursement 
-		ii.	Annual Cleanup reimbursement
-	e.	Payment to Barb and Ed Quire for neighborhood sign repairs $300
+6) Old Business
+a. Bylaw’s Committee
+b. Covenants Enforcement Committee
+c. Building Committee
+d. Insurance policy (updating current policy)
+e. Annual Meeting and Neighborhood cleanup reimbursement
 
-7)	New Business
-	a.	Special Meeting of the Board of Directors summary
-		i.	Board Code of Conduct
-		ii.	Covenants enforcement, policy and implementation plan
-		iii.	Volunteers for Covenants Committee
-		iv.	Covenants Survey
-	b.	Neighbor Complaints form and policy
-	c.	Donation to Snoqualmie Pass Fire and Rescue from 2023-2024 Budget
+7) New Business
+a. Volunteers for Hyak Signage Community
+b. Construction Debris and Clean Up
+c. Proposed Easement Granting to HPOA
+d. Neighborhood Firewising
 
-8)	Communications
+8) Communications
 
-9)	Guest Comments
+9) Guest Comments
 
-10)	Meeting Adjourned 
-	a.	Next Meeting Date: 09/05/2023
+10) Meeting Adjourned
+a. Next Meeting Date: 09/05/2023
 </pre>
             </div>
 
@@ -140,7 +136,7 @@ export default {
     // only show the upcoming agenda until a little after the meeting date (March 7th)
     onTime() {
       const now = moment();
-      const later = moment("2023-08-02 02:00");
+      const later = moment("2023-09-06 02:00");
       return now.isBefore(later);
     },
   },
